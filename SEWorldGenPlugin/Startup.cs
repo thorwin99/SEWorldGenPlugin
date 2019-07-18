0b19b5e21564c7972deade8b51a1c3d3a69a61fb
thorwin99 <thorwin99@gmail.com>
2019-07-18 14:45:24 +0200

    Changed Worldgeneration to respect Gravity of Planets and to check if a Moon would spawn too close to another.

--- SEWorldGenPlugin/Startup.cs
+++ SEWorldGenPlugin/Startup.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Sandbox.Graphics.GUI;
+using SpaceEngineers.Game;
+using Sandbox.Game.Screens;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VRage;
 using VRage.Plugins;
 using VRage.Utils;
+using Sandbox.Game;
 
 namespace SEWorldGenPlugin
 {
