4e16cba1a85f7f8abc3d6ec99ad468ce1d1e3837
Thorwin Vogt <thorwin99@gmail.com>
2020-09-08 18:31:32 +0200

    Revert "Added missing statement to disable the max roid size slider"
    This reverts commit 127631734bd2a5f2d3477339be4f0c4867e6c8cc.

--- SEWorldGenPlugin/GUI/PluginWorldSettings.cs
+++ SEWorldGenPlugin/GUI/PluginWorldSettings.cs
@@ -115,6 +115,7 @@ namespace SEWorldGenPlugin.GUI
                                 {
                                     MySettings.Static.SessionSettings = new MyObjectBuilder_PluginSettings();
                                     MySettings.Static.SessionSettings.Enable = m_enablePlugin.IsChecked;
+                                    MySettings.Static.SessionSettings.GeneratorSettings = MySettings.Static.Settings.GeneratorSettings;
                                 }
                             };
                         }
