9db05dc4744bb8242c1312f39578da2f1cc0670c
Thorwin Vogt <thorwin99@gmail.com>
2020-09-10 17:30:24 +0200

    Added documentation to the SEWorldGenPlugin.Draw.RenderSphere class

--- SEWorldGenPlugin/Draw/RenderSphere.cs
+++ SEWorldGenPlugin/Draw/RenderSphere.cs
@@ -1,16 +1,24 @@
 ﻿using VRage.Game;
 using VRage.Utils;
 using VRageMath;
-using VRageRender;
 
 namespace SEWorldGenPlugin.Draw
 {
+    /// <summary>
+    /// Simple class that can render a sphere into the gameworld.
+    /// </summary>
     public class RenderSphere : IRenderObject
     {
         private Vector3D m_position;
         private float m_radius;
         private Color m_color;
 
+        /// <summary>
+        /// Creates a new sphere at the given position with given radius and color.
+        /// </summary>
+        /// <param name="position">The position of the sphere in the world space</param>
+        /// <param name="radius">The radius of the sphere</param>
+        /// <param name="color">The sphere color</param>
         public RenderSphere(Vector3D position, float radius, Color color)
         {
             m_position = position;
@@ -18,6 +26,10 @@ namespace SEWorldGenPlugin.Draw
             m_color = color;
         }
 
+        /// <summary>
+        /// Draws the sphere in the game world as a solid mesh
+        /// with a visible wireframe.
+        /// </summary>
         public void Draw()
         {
             MatrixD wm = MatrixD.CreateWorld(m_position);
