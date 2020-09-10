851c05209ce68794903eaf54f7751bca23f37de9
Thorwin Vogt <thorwin99@gmail.com>
2020-09-10 17:19:22 +0200

    Added documentation to SEWorldGenPlugin.Draw.IRenderObject

--- SEWorldGenPlugin/Draw/IRenderObject.cs
+++ SEWorldGenPlugin/Draw/IRenderObject.cs
@@ -1,7 +1,14 @@
 ﻿namespace SEWorldGenPlugin.Draw
 {
+    /// <summary>
+    /// An interface that provides a Draw method for
+    /// all classes that can be drawn in the gameworld, such as simple spheres.
+    /// </summary>
     public interface IRenderObject
     {
+        /// <summary>
+        /// The draw method used to draw items into the world.
+        /// </summary>
         void Draw();
     }
 }
