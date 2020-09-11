d4c7b8889d39fa1226f0a76cd8c83a9d3b01045d
Thorwin Vogt <thorwin99@gmail.com>
2020-09-11 15:59:26 +0200

    Documented SEWorldGenPlugin.Generator.ProceduralGen.EntityExtension class

--- SEWorldGenPlugin/Generator/ProceduralGen/EntityExtension.cs
+++ SEWorldGenPlugin/Generator/ProceduralGen/EntityExtension.cs
@@ -3,9 +3,16 @@ using VRage.Game.Entity;
 
 namespace SEWorldGenPlugin.Generator.ProceduralGen
 {
+    /// <summary>
+    /// Extension class for VRage.Game.Entity.MyEntity which holds extension methods.
+    /// </summary>
     public static class EntityExtension
     {
-
+        /// <summary>
+        /// This method adds the MyEntity to the Procedural Generator as a 
+        /// tracked Entity.
+        /// </summary>
+        /// <param name="thisEntity">The Entiy this method executes on</param>
         public static void ProceduralGeneratorTracking(this MyEntity thisEntity)
         {
             if (thisEntity is MyVoxelBase) return;
