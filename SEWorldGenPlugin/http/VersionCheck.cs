c71f8e7c6403d8614f01b7412fa493ae90856cba
Thorwin Vogt <thorwin99@gmail.com>
2020-09-13 15:24:58 +0200

    Documented SEWorldGenPlugin.http.VersionCheck class

--- SEWorldGenPlugin/http/VersionCheck.cs
+++ SEWorldGenPlugin/http/VersionCheck.cs
@@ -1,11 +1,14 @@
 ﻿using SEWorldGenPlugin.http.Responses;
 using SEWorldGenPlugin.Utilities;
 using System;
-using System.IO;
 using System.Net;
 
 namespace SEWorldGenPlugin.http
 {
+    /// <summary>
+    /// Class to request the latest plugin version from github and checking if the current
+    /// version is the latest version. Is a singleton class
+    /// </summary>
     public class VersionCheck
     {
         public static VersionCheck Static;
@@ -14,6 +17,10 @@ namespace SEWorldGenPlugin.http
         string m_latestBuild;
         string m_latestPage;
 
+        /// <summary>
+        /// Creates the instance for this class and fetches the latest
+        /// version from github
+        /// </summary>
         public VersionCheck()
         {
             Static = this;
@@ -21,11 +28,19 @@ namespace SEWorldGenPlugin.http
             GetNewestVersion();
         }
 
+        /// <summary>
+        /// Returns the current installed version
+        /// </summary>
+        /// <returns>The current installed version of this plugin</returns>
         public string GetVersion()
         {
             return m_version;
         }
 
+        /// <summary>
+        /// Fetches the latest plugin version from github and processes the result.
+        /// </summary>
+        /// <returns>The latest version</returns>
         public string GetNewestVersion()
         {
             HttpWebRequest request = WebRequest.Create("https://api.github.com/repos/thorwin99/SEWorldGenPlugin/releases/latest") as HttpWebRequest;
@@ -49,11 +64,19 @@ namespace SEWorldGenPlugin.http
             
         }
 
+        /// <summary>
+        /// Returns the latest version of the plugin
+        /// </summary>
+        /// <returns>The latest version as a string</returns>
         public string GetLatestVersionPage()
         {
             return m_latestPage;
         }
 
+        /// <summary>
+        /// Checks whether the installed plugin version is the latest
+        /// </summary>
+        /// <returns>True, if it is the latest version</returns>
         public bool IsNewest()
         {
             if (m_latestBuild.Contains("pre"))
