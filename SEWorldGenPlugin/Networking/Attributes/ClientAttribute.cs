5ddd4af6bfdfdca388e10c3c716927ae02570b79
Thorwin Vogt <thorwin99@gmail.com>
2020-09-14 21:34:32 +0200

    Documented SEWorldGenPlugin.Networking.Attributes.ClientAttribute

--- SEWorldGenPlugin/Networking/Attributes/ClientAttribute.cs
+++ SEWorldGenPlugin/Networking/Attributes/ClientAttribute.cs
@@ -2,6 +2,10 @@
 
 namespace SEWorldGenPlugin.Networking.Attributes
 {
+    /// <summary>
+    /// Marks a method to be executed on the client by the PluginEventHandler.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
     public class ClientAttribute : Attribute
     {
     }
