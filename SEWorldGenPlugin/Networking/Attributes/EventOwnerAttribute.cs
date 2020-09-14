8f9dc0d5f69ee5740ef52f3f30f66862e0d51c4c
Thorwin Vogt <thorwin99@gmail.com>
2020-09-14 21:38:09 +0200

    Documented SEWorldGenPlugin.Networking.Attributes.EventOwnerAttribute

--- SEWorldGenPlugin/Networking/Attributes/EventOwnerAttribute.cs
+++ SEWorldGenPlugin/Networking/Attributes/EventOwnerAttribute.cs
@@ -2,6 +2,11 @@
 
 namespace SEWorldGenPlugin.Networking.Attributes
 {
+    /// <summary>
+    /// Marks a class as an EventOwner, which means this class contains
+    /// methods that can be executed over network on clients or the server.
+    /// It also means, all network methods in this class will be registered in the PluginEventHandler
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
     public class EventOwnerAttribute : Attribute
     {
