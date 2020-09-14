6d0e3157c762d03c6dff3b56df3b2a83cbe320ed
Thorwin Vogt <thorwin99@gmail.com>
2020-09-14 21:32:47 +0200

    Documented SEWorldGenPlugin.Networking.Attributes.BroadcastAttribute

--- SEWorldGenPlugin/Networking/Attributes/BroadcastAttribute.cs
+++ SEWorldGenPlugin/Networking/Attributes/BroadcastAttribute.cs
@@ -2,6 +2,10 @@
 
 namespace SEWorldGenPlugin.Networking.Attributes
 {
+    /// <summary>
+    /// Attribute, to mark a method as a broadcast method for the
+    /// PluginEventHandler, which sends this event to all clients.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
     public class BroadcastAttribute : Attribute
     {
