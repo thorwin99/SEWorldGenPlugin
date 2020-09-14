5108f18b29fc6cb2e35fe189055e75c2ac731f42
Thorwin Vogt <thorwin99@gmail.com>
2020-09-14 21:36:31 +0200

    Documented SEWorldGenPlugin.Networking.Attributes.EventAttribute

--- SEWorldGenPlugin/Networking/Attributes/EventAttribute.cs
+++ SEWorldGenPlugin/Networking/Attributes/EventAttribute.cs
@@ -2,6 +2,12 @@
 
 namespace SEWorldGenPlugin.Networking.Attributes
 {
+    /// <summary>
+    /// Marks a method as a NetworkEvent to be registered in the PluginEventHandler.
+    /// The method also needs at least the broadcast, client or server attribute, to actually get
+    /// executed. The containing class needs to have the EventOwner attribute to actually register this
+    /// method in the PluginEventHandler
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
     public class EventAttribute : Attribute
     {
