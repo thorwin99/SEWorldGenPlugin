3e59491e4951b529731c0a789b615c0a8e76e31b
Thorwin Vogt <thorwin99@gmail.com>
2020-09-15 17:48:51 +0200

    Documented SEWorldGenPlugin.Utilities.Verifier

--- SEWorldGenPlugin/Utilities/Verifier.cs
+++ SEWorldGenPlugin/Utilities/Verifier.cs
@@ -1,7 +1,19 @@
 ﻿namespace SEWorldGenPlugin.Utilities
 {
+    /// <summary>
+    /// Utility class to verify numerical values by checking min and max,
+    /// and setting it to a default value, if it is out of bounds
+    /// </summary>
     public class Verifier
     {
+        /// <summary>
+        /// Verifies an integer and sets it to def, if it is outside of the min max bounds.
+        /// </summary>
+        /// <param name="min">Minimum value</param>
+        /// <param name="max">Maximum value</param>
+        /// <param name="def">Default value</param>
+        /// <param name="name">Name of the variable</param>
+        /// <param name="value">Reference to the value to check</param>
         public static void VerifyInt(int min, int max, int def, string name, ref int value)
         {
             if (value < min || value > max)
@@ -12,6 +24,14 @@
             }
         }
 
+        /// <summary>
+        /// Verifies a long and sets it to def, if it is outside of the min max bounds.
+        /// </summary>
+        /// <param name="min">Minimum value</param>
+        /// <param name="max">Maximum value</param>
+        /// <param name="def">Default value</param>
+        /// <param name="name">Name of the variable</param>
+        /// <param name="value">Reference to the value to check</param>
         public static void VerifyLong(long min, long max, long def, string name, ref long value)
         {
             if (value < min || value > max)
@@ -22,6 +42,14 @@
             }
         }
 
+        /// <summary>
+        /// Verifies a float and sets it to def, if it is outside of the min max bounds.
+        /// </summary>
+        /// <param name="min">Minimum value</param>
+        /// <param name="max">Maximum value</param>
+        /// <param name="def">Default value</param>
+        /// <param name="name">Name of the variable</param>
+        /// <param name="value">Reference to the value to check</param>
         public static void VerifyFloat(float min, float max, float def, string name, ref float value)
         {
             if (value < min || value > max)
@@ -32,6 +60,14 @@
             }
         }
 
+        /// <summary>
+        /// Verifies a double and sets it to def, if it is outside of the min max bounds.
+        /// </summary>
+        /// <param name="min">Minimum value</param>
+        /// <param name="max">Maximum value</param>
+        /// <param name="def">Default value</param>
+        /// <param name="name">Name of the variable</param>
+        /// <param name="value">Reference to the value to check</param>
         public static void VerifyDouble(double min, double max, double def, string name, ref double value)
         {
             if (value < min || value > max)
