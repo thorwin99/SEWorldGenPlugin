fd77534f5200548c9b1a22585c13b31f2a840de6
Thorwin Vogt <thorwin99@gmail.com>
2020-09-15 16:23:19 +0200

    Documented SEWorldGenPlugin.Session.SettingsSession

--- SEWorldGenPlugin/Session/SettingsSession.cs
+++ SEWorldGenPlugin/Session/SettingsSession.cs
@@ -1,30 +1,39 @@
 ﻿using Sandbox.Game.Multiplayer;
 using SEWorldGenPlugin.ObjectBuilders;
 using SEWorldGenPlugin.Utilities;
-using VRage.Game;
 using VRage.Game.Components;
-using VRage.Utils;
 
 namespace SEWorldGenPlugin.Session
 {
+    /// <summary>
+    /// Sessioncomponent that manages the current sessions plugin settings.
+    /// </summary>
     [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate, 1000)]
     public class SettingsSession : MySessionComponentBase
     {
+        /// <summary>
+        /// Name of the save file in the world folder for the plugin settings.
+        /// </summary>
         public const string FILE_NAME = "worldSettings.xml";
 
+        /// <summary>
+        /// Singleton instance of this session component
+        /// </summary>
         public static SettingsSession Static;
 
+        /// <summary>
+        /// The current sessions plugin settings.
+        /// </summary>
         public MyObjectBuilder_PluginSettings Settings
         {
             get;
             set;
         }
 
-        public override void Init(MyObjectBuilder_SessionComponent sessionComponent)
-        {
-            
-        }
-
+        /// <summary>
+        /// Loads the savefile of the plugin settings of the world and stores it
+        /// int the Settings member variable
+        /// </summary>
         public override void LoadData()
         {
             Static = this;
@@ -39,7 +48,7 @@ namespace SEWorldGenPlugin.Session
                     var s = new MySettings();
                     s.LoadSettings();
                 }
-                if(MySettings.Static.SessionSettings != null)
+                if (MySettings.Static.SessionSettings != null)
                 {
                     Settings = MySettings.Static.SessionSettings;
                 }
@@ -47,17 +56,23 @@ namespace SEWorldGenPlugin.Session
                 {
                     Settings = MySettings.Static.Settings;
                 }
-                
+
                 MySettings.Static.SessionSettings = null;
             }
         }
 
+        /// <summary>
+        /// Saves the current sessions plugin settings to the world folder.
+        /// </summary>
         public override void SaveData()
         {
             if(Sync.IsServer)
                 FileUtils.WriteXmlFileToWorld(Settings, FILE_NAME, typeof(SettingsSession));
         }
 
+        /// <summary>
+        /// Unloads the used data of this session component.
+        /// </summary>
         protected override void UnloadData()
         {
             FileUtils.WriteXmlFileToWorld(Settings, FILE_NAME, typeof(SettingsSession));
