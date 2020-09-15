e0c750d8c654702a60d62e222ee50de276f5fe06
Thorwin Vogt <thorwin99@gmail.com>
2020-09-15 17:42:02 +0200

    Documented SEWorldGenPlugin.Utilities.PluginLog

--- SEWorldGenPlugin/Utilities/PluginLog.cs
+++ SEWorldGenPlugin/Utilities/PluginLog.cs
@@ -2,6 +2,9 @@
 
 namespace SEWorldGenPlugin.Utilities
 {
+    /// <summary>
+    /// Enum to represent the log level of a log message
+    /// </summary>
     public enum LogLevel
     {
         INFO,
@@ -9,8 +12,17 @@ namespace SEWorldGenPlugin.Utilities
         ERROR
     };
 
+    /// <summary>
+    /// Utility class to easily log message to the logfile with different log levels.
+    /// </summary>
     public class PluginLog
     {
+
+        /// <summary>
+        /// Writes a message to the log file with an optional log level
+        /// </summary>
+        /// <param name="message">Message to log</param>
+        /// <param name="level">Optional log level</param>
         public static void Log(string message, LogLevel level = LogLevel.INFO)
         {
             switch (level)
