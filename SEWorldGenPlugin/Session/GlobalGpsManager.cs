83221adfbaef2dce68cdfe7f7b45f1ca22adb5df
Thorwin Vogt <thorwin99@gmail.com>
2020-09-15 16:04:31 +0200

    Documented SEWorldGenPlugin.Session.GlobalGpsManager

--- SEWorldGenPlugin/Session/GlobalGpsManager.cs
+++ SEWorldGenPlugin/Session/GlobalGpsManager.cs
@@ -4,14 +4,22 @@ using SEWorldGenPlugin.ObjectBuilders;
 using SEWorldGenPlugin.Utilities;
 using System.Collections.Generic;
 using VRage.Game.Components;
-using VRage.Game.ModAPI;
 using VRageMath;
 
 namespace SEWorldGenPlugin.Session
 {
+    /// <summary>
+    /// Sessioncomponent that manages global gpss. Global gpss are gpss that should be displayed
+    /// to all players on the server and be permanent. It also manages dynamic gpss which are only displayed to
+    /// one player and can be dynamically updated.
+    /// </summary>
     [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation, 400)]
     public class GlobalGpsManager : MySessionComponentBase
     {
+        /// <summary>
+        /// Struct that represents the id for a dynamic gps. The ID of a dynamic gps is
+        /// made up of the id of the player that the gps is displayed to, and the name of the gps.
+        /// </summary>
         struct DynamicGpsId
         {
             public long playerId;
@@ -24,15 +32,47 @@ namespace SEWorldGenPlugin.Session
             }
         }
 
+        /// <summary>
+        /// File name for the save file containing the global gps info
+        /// </summary>
         private const string FILENAME = "GlobalGpsData.xml";
 
+        /// <summary>
+        /// Singleton GlobalGpsManager instance
+        /// </summary>
         public static GlobalGpsManager Static;
 
+        /// <summary>
+        /// All currently managed global gpss
+        /// </summary>
         private HashSet<MyGps> Gpss;
+
+        /// <summary>
+        /// All gpss that have to be added
+        /// </summary>
         private HashSet<MyGps> ToAddGpss;
+
+        /// <summary>
+        /// Dictionary contains all global gpss that are currently displayed to a player.
+        /// Key is the player id and the value is a hashset containing all gps hashes of the global
+        /// gpss currently shown to the player
+        /// </summary>
         private Dictionary<long, HashSet<int>> AddedGpss;
+
+        /// <summary>
+        /// Dictionary that contains all dynamic gpss.
+        /// The key is the dynamic gps id, while the value is the hash of the gps used
+        /// to modify it.
+        /// </summary>
         private Dictionary<DynamicGpsId, int> DynamicGpss;
 
+        /// <summary>
+        /// Adds a new gps to the global gps to be added list.
+        /// Will be added in the next update of the simulation.
+        /// </summary>
+        /// <param name="name">Name of the gps</param>
+        /// <param name="color">Color of the gps</param>
+        /// <param name="pos">Position of the gps</param>
         public void AddGps(string name, Color color, Vector3D pos)
         {
             MyGps g = new MyGps()
@@ -48,6 +88,9 @@ namespace SEWorldGenPlugin.Session
             ToAddGpss.Add(g);
         }
 
+        /// <summary>
+        /// Loads all existend global and dynamic gpss from the save file
+        /// </summary>
         public override void LoadData()
         {
             MyObjectBuilder_GpsManager builder;
@@ -77,6 +120,9 @@ namespace SEWorldGenPlugin.Session
             Static = this;
         }
 
+        /// <summary>
+        /// Saves all dynamic and global gpss to the save data.
+        /// </summary>
         public override void SaveData()
         {
             MyObjectBuilder_GpsManager builder = new MyObjectBuilder_GpsManager();
@@ -103,6 +149,11 @@ namespace SEWorldGenPlugin.Session
             FileUtils.WriteXmlFileToWorld(builder, FILENAME, typeof(GlobalGpsManager));
         }
 
+        /// <summary>
+        /// Run before the simulation update.
+        /// Adds all to be added gpss to the managed gps list and
+        /// updates all players gpss with this new list.
+        /// </summary>
         public override void UpdateBeforeSimulation()
         {
             if (SettingsSession.Static.Settings.Enable || true)
@@ -131,6 +182,15 @@ namespace SEWorldGenPlugin.Session
             }
         }
 
+        /// <summary>
+        /// Adds or updates a dynamic gps. If the dynamic gps with the given name currently is not
+        /// added to the player with playerId, it will be added as a new one. If it already exists, it will
+        /// be updated.
+        /// </summary>
+        /// <param name="gpsName">Name of the gps</param>
+        /// <param name="playerId">Id of the player</param>
+        /// <param name="position">Position of the gps</param>
+        /// <param name="color">Color of the gps</param>
         public void AddOrUpdateDynamicGps(string gpsName, long playerId, Vector3D position, Color color)
         {
             DynamicGpsId id = new DynamicGpsId(playerId, gpsName);
@@ -170,6 +230,11 @@ namespace SEWorldGenPlugin.Session
             }   
         }
 
+        /// <summary>
+        /// Removes a dynamic gps from a player, if it exists
+        /// </summary>
+        /// <param name="gpsName">Name of the gps</param>
+        /// <param name="playerId">Player id of the player</param>
         public void RemoveDynamicGps(string gpsName, long playerId)
         {
             DynamicGpsId id = new DynamicGpsId(playerId, gpsName);
@@ -180,6 +245,9 @@ namespace SEWorldGenPlugin.Session
             }
         }
 
+        /// <summary>
+        /// Unloads all data used by this session component
+        /// </summary>
         protected override void UnloadData()
         {
             DynamicGpss.Clear();
