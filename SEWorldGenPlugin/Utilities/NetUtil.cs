1d6d1072392099f3906517b152ef0049e7130dc5
Thorwin Vogt <thorwin99@gmail.com>
2020-09-15 17:39:57 +0200

    Documented SEWorldGenPlugin.Utilities.NetUtil

--- SEWorldGenPlugin/Utilities/NetUtil.cs
+++ SEWorldGenPlugin/Utilities/NetUtil.cs
@@ -5,12 +5,32 @@ using System.Collections.Generic;
 
 namespace SEWorldGenPlugin.Utilities
 {
+    /// <summary>
+    /// Utility class for network operation and plugin pingin. Can send and receive messages over network
+    /// and ping a server to check if the plugin is on there.
+    /// </summary>
     public class NetUtil
     {
+        /// <summary>
+        /// Dictionary containing all actions to unregister a registered message handler.
+        /// Needed, because this class encapsulates message handlers with its own retreive the sender id before calling the original message handler. 
+        /// Thats why it stores actions to unregister those new message handlers.
+        /// </summary>
         private static Dictionary<ushort, Action> unregActions;
+
+        /// <summary>
+        /// Handler ID for the ping handler
+        /// </summary>
         private const ushort PING_HANDLER = 2575;
+
+        /// <summary>
+        /// id of the current ping, essentially a ping counter
+        /// </summary>
         private static int pingId;
 
+        /// <summary>
+        /// Creates a new net util instance
+        /// </summary>
         static NetUtil()
         {
             unregActions = new Dictionary<ushort, Action>();
@@ -18,6 +38,13 @@ namespace SEWorldGenPlugin.Utilities
             pingId = 0;
         }
 
+        /// <summary>
+        /// Registers a new message handler  with the given id.
+        /// </summary>
+        /// <param name="id">The handler ID</param>
+        /// <param name="handler">The handler with 2 parameters, a ulong for the sender id and a string for the network message</param>
+        /// Will encapsulate the handler into a new handler, which splits the byte[] message into
+        /// a string message and a sender id, then calls the registered handler.
         public static void RegisterMessageHandler(ushort id, Action<ulong, string> handler)
         {
             Action<byte[]> newHandler = delegate (byte[] data)
@@ -42,6 +69,13 @@ namespace SEWorldGenPlugin.Utilities
             }
         }
 
+        /// <summary>
+        /// Registers a new message handler with the given id.
+        /// </summary>
+        /// <param name="id">The handler ID</param>
+        /// <param name="handler">The handler with 2 parameters, a ulong for the sender id and a byte[] for the network data</param>
+        /// /// Will encapsulate the handler into a new handler, which splits the byte[] message into
+        /// a byte[] data message and a sender id, then calls the registered handler.
         public static void RegisterMessageHandler(ushort id, Action<ulong, byte[]> handler)
         {
             Action<byte[]> newHandler = delegate (byte[] data)
@@ -66,6 +100,10 @@ namespace SEWorldGenPlugin.Utilities
             }
         }
 
+        /// <summary>
+        /// Unregisteres a message handler with the given id, if it is registered.
+        /// </summary>
+        /// <param name="id">Id of the message handler</param>
         public static void UnregisterMessageHandlers(ushort id)
         {
             if(unregActions.TryGetValue(id, out Action value))
@@ -75,6 +113,12 @@ namespace SEWorldGenPlugin.Utilities
             }
         }
 
+        /// <summary>
+        /// Sends a string message to the server, which should get handled by the
+        /// message handler with handlerId.
+        /// </summary>
+        /// <param name="handlerId">Id of the message handler that should handle this message</param>
+        /// <param name="message">Message to send</param>
         public static void SendPacketToServer(ushort handlerId, string message)
         {
             ulong id = MyAPIGateway.Multiplayer.MyId;
@@ -82,6 +126,12 @@ namespace SEWorldGenPlugin.Utilities
             MyAPIGateway.Multiplayer.SendMessageToServer(handlerId, data, true);
         }
 
+        /// <summary>
+        /// Sends a string message to all clients, which should get handled by the
+        /// message handler with handlerId.
+        /// </summary>
+        /// <param name="handlerId">Id of the message handler that should handle this message</param>
+        /// <param name="message">Message to send</param>
         public static void SendPacketToClients(ushort handlerId, string message)
         {
             ulong id = MyAPIGateway.Multiplayer.MyId;
@@ -89,6 +139,13 @@ namespace SEWorldGenPlugin.Utilities
             MyAPIGateway.Multiplayer.SendMessageToOthers(handlerId, data, true);
         }
 
+        /// <summary>
+        /// Sends a string message to the client with the provided id. The message will be handled by
+        /// the message handler that corresponds to the provided handler id.
+        /// </summary>
+        /// <param name="handlerId">Id of the message handler, which will handle the incoming message</param>
+        /// <param name="message">The message</param>
+        /// <param name="receiver">The id of the client, that receives this packet</param>
         public static void SendPacket(ushort handlerId, string message, ulong receiver)
         {
             ulong id = MyAPIGateway.Multiplayer.MyId;
@@ -96,24 +153,49 @@ namespace SEWorldGenPlugin.Utilities
             MyAPIGateway.Multiplayer.SendMessageTo(handlerId, data, receiver, true);
         }
 
+        /// <summary>
+        /// Sends a byte[] of data to the server, which should get handled by the
+        /// message handler with handlerId.
+        /// </summary>
+        /// <param name="handlerId">Id of the message handler that should handle this message</param>
+        /// <param name="data">Data to send</param>
         public static void SendPacketToServer(ushort handlerId, byte[] data)
         {
             ulong id = MyAPIGateway.Multiplayer.MyId;
             MyAPIGateway.Multiplayer.SendMessageToServer(handlerId, PrefixDataWithId(id, data), true);
         }
 
+        /// <summary>
+        /// Sends a byte[] of data to all clients, which should get handled by the
+        /// message handler with handlerId.
+        /// </summary>
+        /// <param name="handlerId">Id of the message handler that should handle this message</param>
+        /// <param name="data">Data to send</param>
         public static void SendPacketToClients(ushort handlerId, byte[] data)
         {
             ulong id = MyAPIGateway.Multiplayer.MyId;
             MyAPIGateway.Multiplayer.SendMessageToOthers(handlerId, PrefixDataWithId(id, data), true);
         }
 
+        /// <summary>
+        /// Sends a byte[] of data to the client corresponding to the provided receiver id. It should get handled by the
+        /// message handler with handlerId.
+        /// </summary>
+        /// <param name="handlerId">Id of the message handler that should handle this message</param>
+        /// <param name="data">Data to send</param>
+        /// <param name="receiver">Id of the client that receives this message</param>
         public static void SendPacket(ushort handlerId, byte[] data, ulong receiver)
         {
             ulong id = MyAPIGateway.Multiplayer.MyId;
             MyAPIGateway.Multiplayer.SendMessageTo(handlerId, PrefixDataWithId(id, data), receiver, true);
         }
 
+        /// <summary>
+        /// Pings the server and calls the callback on success.
+        /// </summary>
+        /// <param name="successCallback">Callback for success</param>
+        /// Registers a new message handler for every ping, because they compare the
+        /// id of the pong with the id of the ping.
         public static void PingServer(Action successCallback)
         {
             int thisId = pingId++;
@@ -127,6 +209,11 @@ namespace SEWorldGenPlugin.Utilities
             SendPacketToServer(PING_HANDLER, GetPingMsg(thisId));
         }
 
+        /// <summary>
+        /// Handles ping messages to and from the server.
+        /// </summary>
+        /// <param name="senderId">Id of the sender, that send the packet</param>
+        /// <param name="msg">Incoming ping message</param>
         private static void ServerPingHandler(ulong senderId, string msg)
         {
             if (!SettingsSession.Static.Settings.Enable) return;
@@ -136,20 +223,42 @@ namespace SEWorldGenPlugin.Utilities
             }
         }
 
+        /// <summary>
+        /// Builds a ping message with the given id
+        /// </summary>
+        /// <param name="id">Id of ping</param>
+        /// <returns>Message of ping</returns>
         private static string GetPingMsg(int id)
         {
-            return "PING" + id;
+            return GetPingRawMsg() + id;
         }
+
+        /// <summary>
+        /// Builds a pong message with the given id
+        /// </summary>
+        /// <param name="id">Id of pong</param>
+        /// <returns>Message of pong</returns>
         private static string GetPongMsg(int id)
         {
             return "PONG" + id;
         }
 
+        /// <summary>
+        /// Returns a raw ping message, without id
+        /// </summary>
+        /// <returns>Ping message</returns>
         private static string GetPingRawMsg()
         {
             return "PING";
         }
 
+        /// <summary>
+        /// Converts a string message to network data. For that, it will prefix the message with the sender id,
+        /// and then append the string message as bytes.
+        /// </summary>
+        /// <param name="senderId">Sender Id</param>
+        /// <param name="msg">String message</param>
+        /// <returns>Byte[] ready to be send over net.</returns>
         public static byte[] MsgToNetData(ulong senderId, string msg)
         {
             char[] chars = msg.ToCharArray();
@@ -170,6 +279,12 @@ namespace SEWorldGenPlugin.Utilities
             return bytes;
         }
 
+        /// <summary>
+        /// Converts a network data to a string message and a sender id.
+        /// </summary>
+        /// <param name="data">The incoming data</param>
+        /// <param name="msg">The output string message contained in data</param>
+        /// <returns>The sender id of the sender</returns>
         public static ulong NetDataToMsg(byte[] data, out string msg)
         {
             ulong id = 0;
@@ -189,6 +304,13 @@ namespace SEWorldGenPlugin.Utilities
             return id;
         }
 
+        /// <summary>
+        /// Reads the sender id from the data and outputs the raw data without the sender id.
+        /// Sender id is returned
+        /// </summary>
+        /// <param name="data">Incoming network data</param>
+        /// <param name="rawData">Output raw data without sender id</param>
+        /// <returns>Sender id of the sender</returns>
         private static ulong ReadSenderId(byte[] data, out byte[] rawData)
         {
             rawData = new byte[data.Length - 8];
@@ -208,6 +330,12 @@ namespace SEWorldGenPlugin.Utilities
             return id;
         }
 
+        /// <summary>
+        /// Prefixes raw network data with the sender id.
+        /// </summary>
+        /// <param name="id">Sender id</param>
+        /// <param name="data">Raw data</param>
+        /// <returns>Network data with sender id prefix</returns>
         private static byte[] PrefixDataWithId(ulong id, byte[] data)
         {
             byte[] newData = new byte[data.Length + 8];
