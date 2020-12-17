6c1c3caa9c22f891f7dc73cf759a54bb47dbac7c
8vogt <8vogt@informatik.uni-hamburg.de>
2020-12-17 16:42:06 +0100

    Added: Implemented global settings file

--- SEWorldGenPlugin/ObjectBuilders/MyObjectBuilder_GlobalSettings.cs
+++ SEWorldGenPlugin/ObjectBuilders/MyObjectBuilder_GlobalSettings.cs
@@ -1,5 +1,6 @@
 ﻿using ProtoBuf;
 using System;
+using System.Collections.Generic;
 
 namespace SEWorldGenPlugin.ObjectBuilders
 {
@@ -9,12 +10,30 @@ namespace SEWorldGenPlugin.ObjectBuilders
     {
         public MyObjectBuilder_GlobalSettings() : base()
         {
-
         }
+
+        [ProtoMember(1)]
+        public HashSet<string> MoonDefinitions = new HashSet<string>();
+
+        [ProtoMember(2)]
+        public HashSet<string> GasGiantDefinitions = new HashSet<string>();
+
+        [ProtoMember(3)]
+        public HashSet<string> BlacklistedPlanetDefinitions = new HashSet<string>();
+
+        [ProtoMember(4)]
+        public HashSet<string> MandatoryPlanetDefinitions = new HashSet<string>();
 
         public override MyAbstractConfigObjectBuilder copy()
         {
-            return new MyObjectBuilder_GlobalSettings();
+            var copy = new MyObjectBuilder_GlobalSettings();
+
+            copy.MoonDefinitions = MoonDefinitions;
+            copy.GasGiantDefinitions = GasGiantDefinitions;
+            copy.BlacklistedPlanetDefinitions = BlacklistedPlanetDefinitions;
+            copy.MandatoryPlanetDefinitions = MandatoryPlanetDefinitions;
+
+            return copy;
         }
 
         public override void Verify()
