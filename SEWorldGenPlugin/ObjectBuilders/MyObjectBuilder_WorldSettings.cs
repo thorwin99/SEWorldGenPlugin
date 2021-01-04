3c0876842d4c3f2c269ee3b7410f82c04d5fa28c
8vogt <8vogt@informatik.uni-hamburg.de>
2021-01-04 16:44:03 +0100

    Refactored MySerializableMinMax into own class file

--- SEWorldGenPlugin/ObjectBuilders/MyObjectBuilder_WorldSettings.cs
+++ SEWorldGenPlugin/ObjectBuilders/MyObjectBuilder_WorldSettings.cs
@@ -206,43 +206,6 @@ namespace SEWorldGenPlugin.ObjectBuilders
     }
 
     /// <summary>
-    /// Serializable struct used to represent a min max pair of values.
-    /// The values are automatically sorted in the constructor, so
-    /// min is always min and max is always max.
-    /// </summary>
-    [ProtoContract]
-    [Serializable]
-    public class MySerializableMinMax : MyAbstractConfigObjectBuilder
-    {
-        [ProtoMember(1)]
-        public int Min;
-
-        [ProtoMember(2)]
-        public int Max;
-
-        public MySerializableMinMax(int v1, int v2)
-        {
-            Min = Math.Min(v1, v2);
-            Max = Math.Max(v1, v2);
-        }
-
-        public override MyAbstractConfigObjectBuilder copy()
-        {
-            return new MySerializableMinMax(Min, Max);
-        }
-
-        public override void Verify()
-        {
-            if(Min > Max)
-            {
-                int t = Max;
-                Max = Min;
-                Min = t;
-            }
-        }
-    }
-
-    /// <summary>
     /// Enum to set which asteroid generator the plugin uses.
     /// </summary>
     public enum AsteroidGenerationMethod
---
+++ SEWorldGenPlugin/ObjectBuilders/MySerializableMinMax.cs
@@ -0,0 +1,42 @@
+﻿using ProtoBuf;
+using System;
+
+namespace SEWorldGenPlugin.ObjectBuilders
+{
+    /// <summary>
+    /// Serializable struct used to represent a min max pair of values.
+    /// The values are automatically sorted in the constructor, so
+    /// min is always min and max is always max.
+    /// </summary>
+    [ProtoContract]
+    [Serializable]
+    public class MySerializableMinMax : MyAbstractConfigObjectBuilder
+    {
+        [ProtoMember(1)]
+        public int Min;
+
+        [ProtoMember(2)]
+        public int Max;
+
+        public MySerializableMinMax(int v1, int v2)
+        {
+            Min = Math.Min(v1, v2);
+            Max = Math.Max(v1, v2);
+        }
+
+        public override MyAbstractConfigObjectBuilder copy()
+        {
+            return new MySerializableMinMax(Min, Max);
+        }
+
+        public override void Verify()
+        {
+            if (Min > Max)
+            {
+                int t = Max;
+                Max = Min;
+                Min = t;
+            }
+        }
+    }
+}
