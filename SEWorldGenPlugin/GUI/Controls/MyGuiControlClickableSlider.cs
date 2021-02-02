2a59b2a09073e42ca4b5297d524807e1af8ea59c
8vogt <8vogt@informatik.uni-hamburg.de>
2021-02-02 18:24:18 +0100

    Changed the clickable slider to have the ability to display a value label with suffix and prefix as well as a listener, for when the text was updated, so that it can be changed by classes that use the slider as a gui element

--- SEWorldGenPlugin/GUI/Controls/MyGuiControlClickableSlider.cs
+++ SEWorldGenPlugin/GUI/Controls/MyGuiControlClickableSlider.cs
@@ -1,5 +1,6 @@
 ﻿using Sandbox;
 using Sandbox.Graphics.GUI;
+using System;
 using VRage.Input;
 using VRage.Utils;
 using VRageMath;
@@ -9,13 +10,75 @@ namespace SEWorldGenPlugin.GUI.Controls
     /// <summary>
     /// A GUI Control element, that is a slider to input
     /// numerical values and when clicked while holding ctrl
-    /// it will open an input box, to input an exact number
+    /// it will open an input box, to input an exact number.
+    /// Also changes the label to always display the value of the slider.
     /// </summary>
     public class MyGuiControlClickableSlider : MyGuiControlSlider
     {
-        public MyGuiControlClickableSlider(Vector2? position = null, float minValue = 0f, float maxValue = 1f, float width = 0.29f, float? defaultValue = null, Vector4? color = null, string labelText = null, int labelDecimalPlaces = 1, float labelScale = 0.8f, float labelSpaceWidth = 0f, string labelFont = "White", string toolTip = null, MyGuiControlSliderStyleEnum visualStyle = MyGuiControlSliderStyleEnum.Default, MyGuiDrawAlignEnum originAlign = MyGuiDrawAlignEnum.HORISONTAL_CENTER_AND_VERTICAL_CENTER, bool intValue = false, bool showLabel = false)
-            : base(position, minValue, maxValue, width, defaultValue, color, labelText, labelDecimalPlaces, labelScale, labelSpaceWidth, labelFont, toolTip, visualStyle, originAlign, intValue, showLabel)
+
+        /// <summary>
+        /// Called after the label updated.
+        /// </summary>
+        public Action<MyGuiControlLabel> OnLabelUpdate;
+
+        /// <summary>
+        /// The new label to display the value of this slider
+        /// </summary>
+        MyGuiControlLabel m_label;
+
+        /// <summary>
+        /// The space between label and slider
+        /// </summary>
+        private float m_labelSpaceWidth;
+
+        /// <summary>
+        /// The prefix for the label
+        /// </summary>
+        private string m_labelPrefix;
+
+        /// <summary>
+        /// The suffix for the label
+        /// </summary>
+        private string m_labelSuffix;
+
+        /// <summary>
+        /// Creates a new clickable slider
+        /// </summary>
+        /// <param name="position">Position of slider</param>
+        /// <param name="minValue">Minimum value of slider</param>
+        /// <param name="maxValue">Maximum value of slider</param>
+        /// <param name="width">Width of slider in screen space</param>
+        /// <param name="defaultValue">Default value of slider</param>
+        /// <param name="color">Color of slider</param>
+        /// <param name="labelPrefix">Prefix for the value label</param>
+        /// <param name="labelSuffix">Suffix for the value label</param>
+        /// <param name="labelScale">Scale of the value label</param>
+        /// <param name="labelSpaceWidth">Width of the value label</param>
+        /// <param name="labelFont">Font of the value label</param>
+        /// <param name="toolTip">Tooltip of this slider</param>
+        /// <param name="visualStyle">VisualStyle</param>
+        /// <param name="originAlign">Alignment of origin</param>
+        /// <param name="intValue">Is int value</param>
+        /// <param name="showLabel">Show the value label</param>
+        public MyGuiControlClickableSlider(Vector2? position = null, float minValue = 0f, float maxValue = 1f, float width = 0.29f, float? defaultValue = null, Vector4? color = null, string labelPrefix = "", string labelSuffix = "", float labelScale = 0.8f, float labelSpaceWidth = 0f, string labelFont = "White", string toolTip = null, MyGuiControlSliderStyleEnum visualStyle = MyGuiControlSliderStyleEnum.Default, MyGuiDrawAlignEnum originAlign = MyGuiDrawAlignEnum.HORISONTAL_CENTER_AND_VERTICAL_CENTER, bool intValue = false, bool showLabel = false)
+            : base(position, minValue, maxValue, width, defaultValue, color, null, 1, labelScale, labelSpaceWidth, labelFont, toolTip, visualStyle, originAlign, intValue, false)
         {
+            m_labelSpaceWidth = labelSpaceWidth;
+            m_labelPrefix = labelPrefix;
+            m_labelSuffix = labelSuffix;
+            m_label = new MyGuiControlLabel(null, null, "", null, labelScale, labelFont, MyGuiDrawAlignEnum.HORISONTAL_LEFT_AND_VERTICAL_CENTER);
+            if (showLabel)
+            {
+                Elements.Add(m_label);
+            }
+            m_label.Position = new Vector2(GetPositionAbsoluteCenterLeft().X + Size.X + m_labelSpaceWidth, GetPositionAbsoluteCenterLeft().Y);
+
+            ValueChanged += delegate (MyGuiControlSlider slider)
+            {
+                UpdateValueLabel();
+            };
+
+            UpdateValueLabel();
         }
 
         /// <summary>
@@ -43,6 +106,23 @@ namespace SEWorldGenPlugin.GUI.Controls
         {
             Value = value;
             ValueChanged(this);
+        }
+
+        /// <summary>
+        /// Updates the label
+        /// </summary>
+        private void UpdateValueLabel()
+        {
+            if (IntValue)
+            {
+                m_label.Text = String.Format(m_labelPrefix + "{0:0}" + m_labelSuffix, (int)Value);
+            }
+            else
+            {
+                m_label.Text = String.Format(m_labelPrefix + "{0:0.00}" + m_labelSuffix, Value);
+            }
+
+            OnLabelUpdate?.Invoke(m_label);
         }
     }
 }
