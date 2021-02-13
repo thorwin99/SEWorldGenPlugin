69e2e19c3c1278838d9e0c86279720e72334e8eb
8vogt <8vogt@informatik.uni-hamburg.de>
2021-02-13 14:49:07 +0100

    Added helper class to create gui elements

---
+++ SEWorldGenPlugin/GUI/MyPluginGuiHelper.cs
@@ -0,0 +1,50 @@
+using Sandbox.Game.Gui;
+using Sandbox.Graphics.GUI;
+using System;
+using System.Text;
+using VRage.Game;
+using VRageMath;
+
+namespace SEWorldGenPlugin.GUI
+{
+    /// <summary>
+    /// Helper class for creating various preset gui elements.
+    /// </summary>
+    public class MyPluginGuiHelper
+    {
+
+        /// <summary>
+        /// Creates a debug style button. It is smaller than a nornmal button.
+        /// </summary>
+        /// <param name="usableWidth">Usable width of the button</param>
+        /// <param name="text">Text of the button</param>
+        /// <param name="onClick">On click event of the button</param>
+        /// <param name="enabled">If it is enabled</param>
+        /// <param name="tooltip">Tooltip for the button</param>
+        /// <returns>The created button</returns>
+        public static MyGuiControlButton CreateDebugButton(float usableWidth, string text, Action<MyGuiControlButton> onClick, bool enabled = true, string tooltip = null)
+        {
+            MyGuiControlButton myGuiControlButton = new MyGuiControlButton(null, VRage.Game.MyGuiControlButtonStyleEnum.Debug, onButtonClick: onClick);
+            myGuiControlButton.VisualStyle = MyGuiControlButtonStyleEnum.Rectangular;
+            myGuiControlButton.Size = new Vector2(usableWidth, myGuiControlButton.Size.Y);
+            myGuiControlButton.Enabled = enabled;
+            if (tooltip == null)
+            {
+                myGuiControlButton.SetToolTip(tooltip);
+            }
+            return myGuiControlButton;
+        }
+
+        /// <summary>
+        /// Displays an error message box to the user and calls the given callback, when it is closed.
+        /// </summary>
+        /// <param name="error">The error message</param>
+        /// <param name="caption">The caption of the box</param>
+        /// <param name="callback">Callback to call when closed</param>
+        public static void DisplayError(string error, string caption, Action callback = null)
+        {
+            MyGuiScreenMessageBox message = new MyGuiScreenMessageBox(MyMessageBoxStyleEnum.Error, MyMessageBoxButtonsType.OK, new StringBuilder(error), new StringBuilder(caption), MyCommonTexts.Ok, MyCommonTexts.Cancel, MyCommonTexts.Yes, MyCommonTexts.No, null, 10000, MyGuiScreenMessageBox.ResultEnum.CANCEL, true, null, onClosing: callback); ;
+            MyGuiSandbox.AddScreen(message);
+        }
+    }
+}
