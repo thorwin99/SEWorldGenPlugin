856f4f4b40c06e444da99f337403d2a29b3bf41b
8vogt <8vogt@informatik.uni-hamburg.de>
2021-02-15 15:00:14 +0100

    Added provider and data for hollow sphere asteroid objects

---
+++ SEWorldGenPlugin/Generator/AsteroidObjects/AsteroidSphere/MyAsteroidSphereProvider.cs
@@ -0,0 +1,74 @@
+using ProtoBuf;
+using SEWorldGenPlugin.Generator.AsteroidObjectShapes;
+using SEWorldGenPlugin.GUI.AdminMenu;
+using SEWorldGenPlugin.ObjectBuilders;
+using System;
+
+namespace SEWorldGenPlugin.Generator.AsteroidObjects.AsteroidSphere
+{
+    /// <summary>
+    /// Provider class for a hollow sphere asteroid object
+    /// </summary>
+    public class MyAsteroidSphereProvider : MyAbstractAsteroidObjectProvider
+    {
+        public override MySystemAsteroids GenerateInstance(int systemIndex, in MySystemObject systemParent, double objectOrbitRadius)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IMyAsteroidObjectShape GetAsteroidObjectShape(MySystemAsteroids instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetTypeName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsSystemGeneratable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnSave()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RemoveInstance(MySystemAsteroids systemInstance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool TryLoadObject(MySystemAsteroids asteroid)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IMyAsteroidAdminMenuCreator CreateAdminMenuCreatorInstance()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Class for additional data to save an asteroid object hollow sphere
+    /// </summary>
+    [ProtoContract]
+    [Serializable]
+    public class MyAsteroidSphereData
+    {
+        /// <summary>
+        /// The inner radius for the hollow sphere of asteroids
+        /// </summary>
+        [ProtoMember(1)]
+        public double InnerRadius;
+
+        /// <summary>
+        /// The outer radius for the hollow sphere asteroids
+        /// </summary>
+        [ProtoMember(2)]
+        public double OuterRadius;
+    }
+}
