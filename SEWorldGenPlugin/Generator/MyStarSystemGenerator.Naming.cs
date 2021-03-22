aea5d7e98bd61e9304b561fcb5885e68023f0e75
8vogt <8vogt@informatik.uni-hamburg.de>
2021-03-22 15:40:08 +0100

    Changed planetparent name property to simply parentname since it would work better with the system hierarchy

--- SEWorldGenPlugin/Generator/MyStarSystemGenerator.Naming.cs
+++ SEWorldGenPlugin/Generator/MyStarSystemGenerator.Naming.cs
@@ -1,10 +1,18 @@
-﻿using SEWorldGenPlugin.ObjectBuilders;
-using SEWorldGenPlugin.Utilities;
+﻿using SEWorldGenPlugin.Utilities;
 
 namespace SEWorldGenPlugin.Generator
 {
     public partial class MyStarSystemGenerator
     {
+        public static readonly string PROP_OBJ_NUMBER = "ObjectNumber";
+        public static readonly string PROP_OBJ_NUMBER_GREEK = "ObjectNumberGreek";
+        public static readonly string PROP_OBJ_NUMBER_ROMAN = "ObjectNumberRoman";
+        public static readonly string PROP_OBJ_LETTER_LOWER = "ObjectLetterLower";
+        public static readonly string PROP_OBJ_LETTER_UPPER = "ObjectLetterUpper";
+        public static readonly string PROP_OBJ_ID = "ObjectId";
+        public static readonly string PROP_OBJ_PARENT = "ParentName";
+
+
         /// <summary>
         /// Generates a name for the planet based on the naming scheme
         /// defined in the global settings file
@@ -16,12 +24,12 @@ namespace SEWorldGenPlugin.Generator
         {
             string namingScheme = MySettings.Static.Settings.PlanetNameFormat;
 
-            string name = namingScheme.SetProperty("ObjectNumber", planetIndex + 1)
-                                      .SetProperty("ObjectNumberGreek", MyNamingUtils.GREEK_LETTERS[planetIndex % MyNamingUtils.GREEK_LETTERS.Length])
-                                      .SetProperty("ObjectNumberRoman", MyNamingUtils.ConvertNumberToRoman(planetIndex + 1))
-                                      .SetProperty("ObjectLetterLower", (char)('a' + (planetIndex % 26)))
-                                      .SetProperty("ObjectLetterUpper", (char)('A' + (planetIndex % 26)))
-                                      .SetProperty("ObjectId", subtypeId);
+            string name = namingScheme.SetProperty(PROP_OBJ_NUMBER, planetIndex + 1)
+                                      .SetProperty(PROP_OBJ_NUMBER_GREEK, MyNamingUtils.GREEK_LETTERS[planetIndex % MyNamingUtils.GREEK_LETTERS.Length])
+                                      .SetProperty(PROP_OBJ_NUMBER_ROMAN, MyNamingUtils.ConvertNumberToRoman(planetIndex + 1))
+                                      .SetProperty(PROP_OBJ_LETTER_LOWER, (char)('a' + (planetIndex % 26)))
+                                      .SetProperty(PROP_OBJ_LETTER_UPPER, (char)('A' + (planetIndex % 26)))
+                                      .SetProperty(PROP_OBJ_ID, subtypeId);
 
             return name;
         }
@@ -38,13 +46,13 @@ namespace SEWorldGenPlugin.Generator
         {
             string namingScheme = MySettings.Static.Settings.MoonNameFormat;
 
-            string name = namingScheme.SetProperty("ObjectNumber", moonIndex + 1)
-                                      .SetProperty("ObjectNumberGreek", MyNamingUtils.GREEK_LETTERS[moonIndex % MyNamingUtils.GREEK_LETTERS.Length])
-                                      .SetProperty("ObjectNumberRoman", MyNamingUtils.ConvertNumberToRoman(moonIndex + 1))
-                                      .SetProperty("ObjectLetterLower", (char)('a' + (moonIndex % 26)))
-                                      .SetProperty("ObjectLetterUpper", (char)('A' + (moonIndex % 26)))
-                                      .SetProperty("ObjectId", subtypeId)
-                                      .SetProperty("MoonPlanetName", parentPlanetName);
+            string name = namingScheme.SetProperty(PROP_OBJ_NUMBER, moonIndex + 1)
+                                      .SetProperty(PROP_OBJ_NUMBER_GREEK, MyNamingUtils.GREEK_LETTERS[moonIndex % MyNamingUtils.GREEK_LETTERS.Length])
+                                      .SetProperty(PROP_OBJ_NUMBER_ROMAN, MyNamingUtils.ConvertNumberToRoman(moonIndex + 1))
+                                      .SetProperty(PROP_OBJ_LETTER_LOWER, (char)('a' + (moonIndex % 26)))
+                                      .SetProperty(PROP_OBJ_LETTER_UPPER, (char)('A' + (moonIndex % 26)))
+                                      .SetProperty(PROP_OBJ_ID, subtypeId)
+                                      .SetProperty(PROP_OBJ_PARENT, parentPlanetName);
 
             return name;
         }
