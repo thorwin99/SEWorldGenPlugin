d6c8abf3180c49bdc99ee7f79a262f5fbd7cd672
8vogt <8vogt@informatik.uni-hamburg.de>
2021-07-19 14:03:26 +0200

    Created a plugin session class to manage session specific, plugin related data for the other session components and classes.

---
+++ SEWorldGenPlugin/Session/MyPluginSession.cs
@@ -0,0 +1,35 @@
+﻿using SEWorldGenPlugin.http;
+using VRage.Game;
+using VRage.Game.Components;
+
+namespace SEWorldGenPlugin.Session
+{
+    /// <summary>
+    /// Session component to compare server and client side of the plugin (e.g. version)
+    /// </summary>
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate, 1005)]
+    public class MyPluginSession : MySessionComponentBase
+    {
+        /// <summary>
+        /// Whether the client and server version of the plugin match up.
+        /// </summary>
+        public bool ServerVersionMatch
+        {
+            get;
+            private set;
+        }
+
+        public MyPluginSession()
+        {
+            ServerVersionMatch = false;
+        }
+
+        public override void Init(MyObjectBuilder_SessionComponent sessionComponent)
+        {
+            VersionCheck.Static.CompareVersionWithServer((res) =>
+            {
+                ServerVersionMatch = res;
+            });
+        }
+    }
+}
