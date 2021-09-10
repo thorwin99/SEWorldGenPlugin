22c6d88585e67ca1610e20745f0844f940d23a55
Thorwin Vogt <thorwin99@gmail.com>
2021-09-10 17:14:38 +0200

    Disabling teleport button now when a value was edited. (#90)

--- SEWorldGenPlugin/Generator/AsteroidObjects/AsteroidRing/MyStarSystemDesignerAsteroidRingMenu.cs
+++ SEWorldGenPlugin/Generator/AsteroidObjects/AsteroidRing/MyStarSystemDesignerAsteroidRingMenu.cs
@@ -201,6 +201,8 @@ namespace SEWorldGenPlugin.Generator.AsteroidObjects.AsteroidRing
             roid.AsteroidSize.Min = (long)m_asteroidSizesSlider.CurrentMin;
             roid.AsteroidSize.Max = (long)m_asteroidSizesSlider.CurrentMax;
 
+            m_teleportToRingButton.Enabled = false;
+
             ChangedObject();
         }
 
