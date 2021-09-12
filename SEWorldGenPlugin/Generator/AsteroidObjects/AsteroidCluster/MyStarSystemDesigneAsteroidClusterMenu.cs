6576b4b39e857c9de55dcf83fea91bf124bfe66b
Thorwin Vogt <thorwin99@gmail.com>
2021-09-12 13:24:14 +0200

    Added teleport button for asteroid clusters Fixed cluster slider width

--- SEWorldGenPlugin/Generator/AsteroidObjects/AsteroidCluster/MyStarSystemDesigneAsteroidClusterMenu.cs
+++ SEWorldGenPlugin/Generator/AsteroidObjects/AsteroidCluster/MyStarSystemDesigneAsteroidClusterMenu.cs
@@ -1,8 +1,17 @@
-﻿using Sandbox.Graphics.GUI;
+﻿using Sandbox.Engine.Multiplayer;
+using Sandbox.Game.Gui;
+using Sandbox.Game.World;
+using Sandbox.Graphics.GUI;
+using SEWorldGenPlugin.Generator.AsteroidObjects.AsteroidRing;
+using SEWorldGenPlugin.Generator.AsteroidObjectShapes;
+using SEWorldGenPlugin.GUI;
+using SEWorldGenPlugin.GUI.AdminMenu;
 using SEWorldGenPlugin.GUI.AdminMenu.SubMenus.StarSystemDesigner;
 using SEWorldGenPlugin.GUI.Controls;
 using SEWorldGenPlugin.ObjectBuilders;
 using SEWorldGenPlugin.Session;
+using SEWorldGenPlugin.Utilities;
+using VRageMath;
 
 namespace SEWorldGenPlugin.Generator.AsteroidObjects.AsteroidCluster
 {
@@ -25,6 +34,11 @@ namespace SEWorldGenPlugin.Generator.AsteroidObjects.AsteroidCluster
         /// </summary>
         private MyGuiControlClickableSlider m_sizeSlider;
 
+        /// <summary>
+        /// Button used to teleport to the ring
+        /// </summary>
+        private MyGuiControlButton m_teleportButton;
+
         public MyStarSystemDesigneAsteroidClusterMenu(MySystemAsteroids instance, MyAsteroidClusterData data) : base(instance)
         {
             Data = data;
@@ -34,7 +48,7 @@ namespace SEWorldGenPlugin.Generator.AsteroidObjects.AsteroidCluster
         {
             base.RecreateControls(controlTable, maxWidth, isEditing);
 
-            m_sizeSlider = new MyGuiControlClickableSlider(null, 0, MySettingsSession.Static.Settings.GeneratorSettings.MinMaxOrbitDistance.Min / 20, maxWidth, (float)Data.Size, labelSuffix: " m");
+            m_sizeSlider = new MyGuiControlClickableSlider(null, 0, MySettingsSession.Static.Settings.GeneratorSettings.MinMaxOrbitDistance.Min / 20, maxWidth - 0.1f, (float)Data.Size, labelSuffix: " m");
             m_sizeSlider.ValueChanged += delegate (MyGuiControlSlider s)
             {
                 Data.Size = s.Value;
@@ -42,6 +56,36 @@ namespace SEWorldGenPlugin.Generator.AsteroidObjects.AsteroidCluster
             };
 
             controlTable.AddTableRow(m_sizeSlider);
+
+            m_teleportButton = MyPluginGuiHelper.CreateDebugButton("Teleport to cluster", OnTeleport, true, "Teleports you into the cluster");
+            m_teleportButton.Enabled = isEditing;
+            m_teleportButton.Size = new Vector2(maxWidth, m_teleportButton.Size.Y);
+
+            controlTable.AddTableRow(m_teleportButton);
+        }
+
+        /// <summary>
+        /// Teleports the player into the ring
+        /// </summary>
+        /// <param name="btn"></param>
+        private void OnTeleport(MyGuiControlButton btn)
+        {
+            MyPluginLog.Debug("Teleporting player to " + m_object.DisplayName);
+
+            if (MySession.Static.CameraController != MySession.Static.LocalCharacter || true)
+            {
+                IMyAsteroidObjectShape shape = MyAsteroidRingProvider.Static.GetAsteroidObjectShape(m_object as MySystemAsteroids);
+                if (shape == null)
+                {
+                    MyPluginGuiHelper.DisplayError("Cant teleport to the asteroid cluster. It does not exist", "Error");
+                    return;
+                }
+
+                MyAdminMenuExtension.Static.CloseScreenNow();
+
+                MyMultiplayer.TeleportControlledEntity(shape.GetPointInShape());
+                MyGuiScreenGamePlay.SetCameraController();
+            }
         }
     }
 }
